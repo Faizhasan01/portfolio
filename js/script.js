1978123acfcd44ed5afcfeb2d1a11b1161bceb4c
faiz <faizhasan444@gamil.com>
2026-01-29 17:57:34 +0530

    improve design

---
+++ js/script.js
@@ -0,0 +1,34 @@
+//typewriter
+const textElement = document.querySelector(".typewriter");
+const words = ["Web Developer", "Student", "Tech Enthusiast", "Creative Thinker"];
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+let typeSpeed = 100;
+
+function type() {
+    const currentWord = words[wordIndex];
+    
+    if (isDeleting) {
+        textElement.textContent = currentWord.substring(0, charIndex - 1);
+        charIndex--;
+        typeSpeed = 50;
+    } else {
+        textElement.textContent = currentWord.substring(0, charIndex + 1);
+        charIndex++;
+        typeSpeed = 150;
+    }
+
+    if (!isDeleting && charIndex === currentWord.length) {
+        isDeleting = true;
+        typeSpeed = 2000; 
+    } else if (isDeleting && charIndex === 0) {
+        isDeleting = false;
+        wordIndex = (wordIndex + 1) % words.length;
+        typeSpeed = 500; 
+    }
+
+    setTimeout(type, typeSpeed);
+}
+
+document.addEventListener("DOMContentLoaded", type);
