ec575ad79dd04250b4059b176113e5608db8c26d
faiz <faizhasan444@gamil.com>
2026-01-29 21:14:01 +0530

    design project.html

--- js/script.js
+++ js/script.js
@@ -33,4 +33,30 @@ function type() {
 
 document.addEventListener("DOMContentLoaded", type);
 
+// card
+const cards = document.querySelectorAll(".tilt-card");
 
+cards.forEach(card => {
+    card.addEventListener("mousemove", (e) => {
+        const rect = card.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        
+        
+        const centerX = rect.width / 2;
+        const centerY = rect.height / 2;
+        
+        const rotateX = ((y - centerY) / centerY) * -20;
+        const rotateY = ((x - centerX) / centerX) * 20;
+
+        card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+        
+        
+        card.style.setProperty('--x', `${x}px`);
+        card.style.setProperty('--y', `${y}px`);
+    });
+
+    card.addEventListener("mouseleave", () => {
+        card.style.transform = "perspective(1000px) rotateX(0) rotateY(0)";
+    });
+});
